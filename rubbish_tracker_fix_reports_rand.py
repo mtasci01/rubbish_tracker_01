5102681e2e8861fdb1e046e15136d344b016a94a
Marco Tascini <tascinimarco@tiscali.it>
2024-05-18 01:37:33 +0200

    findSlopeYIntercept

--- rubbish_tracker_fix_reports_rand.py
+++ rubbish_tracker_fix_reports_rand.py
@@ -35,7 +35,16 @@ for report in reports:
 
 x = np.array(x)
 y = np.array(y)
+
+def findSlopeYIntercept(x1,y1,x2,y2):
+    m = (y2-y1)/(x2-x1)
+    b = y2 - m*x2
+    return m,b
+
+print(findSlopeYIntercept(x.min(), y.max(), x.max(), y.min()))
+
 plt.scatter(x,y,color='red')
+plt.axline((x.min(), y.max()), (x.max(), y.min()))
 plt.show()             
 
 
