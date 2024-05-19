a4ae94e92b5ad9bd93fdb7c0599623b36265298e
Marco Tascini <tascinimarco@tiscali.it>
2024-05-19 19:34:33 +0200

    main plot reports

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
 from service.rubbish_tracker_service import RubbishTrackerService
 
 service = RubbishTrackerService()
@@ -11,9 +13,10 @@ print("2. Download areas")
 print("3. Print single report")
 print("4. Print single area")
 print("5. Download Image")
+print("6. Plot Live Reports")
+print("7. Plot Live Reports Day by Day")
 
-val = input("Enter your choice: ") 
-if val == "1":
+def downloadReports():
     print("Choose: 1. All reports; 2. Live Reports; 3. Fixed Reports")
     val2 = input("Enter your choice: ")
     reports=[]
@@ -28,11 +31,13 @@ if val == "1":
     print("found num reports: " + str(len(reports)))     
     filename='reports_out_' + str(service.getRightnowUTC()) + '.json'
     service.writeFile(filename,reports)
-elif val == "2":
+
+def downloadAreas():
     areas = service.getAreas()
     filename='areas_out_' + str(service.getRightnowUTC()) + '.json'
     service.writeFile(filename,areas)
-elif val == "3":
+
+def printSingleReport():
     reportId = input("Enter the report id: ")
     report = service.getReport(reportId)
     print("Lat: " + str(report['lat']))
@@ -42,19 +47,81 @@ elif val == "3":
     print("created at utc: " + service.printEpoch(report["createdAtUTC"]))
     if ("fixedAtUTC" in report):
         print("fixed at utc: " + service.printEpoch(report["fixedAtUTC"]))
-elif val == "4":
+
+def printSingleArea():
     areaId = input("Enter the area id: ")
     area = service.getArea(areaId)
     print("areaname: " + str(area['areaName']))
     print("points: " + str(area['points']))
     print("saved at utc: " + str(area['savedAtUTC']))
     print("polygon area: " + str(area['polyArea']))
-elif val == "5":
+
+def downloadImage():
     imgId = input("Enter the image id: ")
     imgObj = service.downloadReportImg(imgId)
     theBytes = imgObj["imgBytes"]
     filename = str(service.getRightnowUTC()) + "_" + imgObj["filename"]
-    service.writeBinaryFile(filename,theBytes)        
+    service.writeBinaryFile(filename,theBytes)
+
+def plotLiveReports():
+    points = []
+    livereports= service.getLiveReports()
+    for json in livereports:
+        point = [json["lon"],json["lat"]]
+        points.append(point)
+    npPoints= np.array(points)
+    plt.scatter(npPoints[:,0],npPoints[:, 1],color='red')
+    plt.show()
+
+def plotReportsDayByDay():
+    l = service.getLiveReportsByDay()
+    dataObjList = []
+
+    for o in l:
+        year = str(o["_id"]["year"])
+        month = str(o["_id"]["month"])
+        if len(month) == 1:
+            month = "0"+month
+        day = str(o["_id"]["day"])
+        if len(day) == 1:
+            day = "0"+day          
+        datestr = year + "-" + month + "-" + day
+        dataObj = {"datestr":datestr,"totaldocs":o['totaldocs'],"dateint":int(datestr.replace('-',''))}
+        dataObjList.append(dataObj)
+
+    dataObjList.sort(key=lambda x: x['dateint'], reverse=False)    
+
+    date_time_xs = []
+    ys = []
+    for dataObj in dataObjList:
+        date_time_xs.append(dataObj['datestr'])
+        ys.append(dataObj['totaldocs'])
+    date_time_xs = pd.to_datetime(date_time_xs)
+
+    DF = pd.DataFrame()
+    DF['value'] = ys
+    DF = DF.set_index(date_time_xs)
+    plt.plot(DF)
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+val = input("Enter your choice: ") 
+if val == "1":
+    downloadReports()
+elif val == "2":
+    downloadAreas()
+elif val == "3":
+    printSingleReport()
+elif val == "4":
+    printSingleArea()
+elif val == "5":
+    downloadImage()
+elif val == "6":
+   plotLiveReports()
+elif val == "7":
+   plotReportsDayByDay()   
 else:
     print("Unknown choice. Exiting now")
+
+
  
